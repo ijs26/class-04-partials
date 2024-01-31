fe2ad9108610bb772c2608981304fff2c7bc9e64
ijs26 <157386129+ijs26@users.noreply.github.com>
2024-01-31 14:55:00 +0000

    finished lool

--- includes/meta.php
+++ includes/meta.php
@@ -1,8 +1,8 @@
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>TODO: ??? </title>
+    <title> <?php echo $title; ?> - INFO 2300 </title>
 
-  <link rel="stylesheet" type="text/css" href="styles/site.css">
+    <link rel="stylesheet" type="text/css" href="styles/site.css">
 </head>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php $title = "Home"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 
 <?php include("includes/meta.php") ?>
 
